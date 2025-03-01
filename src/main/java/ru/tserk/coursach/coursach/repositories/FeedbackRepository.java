37b6be563b0422efc5a96372b98386d45b9cec74
Alexander <Topchic525@gmail.com>
2025-03-01 10:06:50 +0300

    Удалены не нужные репозитории

--- src/main/java/ru/tserk/coursach/coursach/repositories/FeedbackRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.tserk.coursach.coursach.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.tserk.coursach.coursach.models.Feedback;
-
-@Repository
-public interface FeedbackRepository extends JpaRepository<Feedback, Integer> {
-}
